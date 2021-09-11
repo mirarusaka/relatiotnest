24be891fdcea0a9e10de0e988ff006cef64f36a5
69safe <endokun32@yahoo.co.jp>
2021-09-11 22:04:41 +0900

    Auth追加、messageテーブル作成

--- RelationTest/app/Models/Board.php
+++ RelationTest/app/Models/Board.php
@@ -12,13 +12,12 @@ class Board extends Model //勝手にテーブルと連携してくれている
     protected $guarded = array('id'); //更新不可のカラムを指定
     protected $table = 'test'; //テーブルを手動で指定
 
-
-    // //バリデーションルールを指定
-    // public static $rules = array(
-    //     'person_id' => 'required',
-    //     'title' => 'required',
-    //     'message' => 'required'
-    // );
+    //バリデーションルールを指定
+    public static $rules = array(
+        'person_id' => 'required',
+        'title' => 'required',
+        'message' => 'required'
+    );
 
     //ゲッター これがviewファイルに呼び出せる。
     //この場合、呼び出すと『id：タイトル』となる。
