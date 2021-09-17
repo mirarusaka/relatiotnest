dee555e31ad4fde56cc4ef88bf6cb482162e75f2
69safe <endokun32@yahoo.co.jp>
2021-09-17 12:03:51 +0900

    テーブル基礎用意

--- Board/database/migrations/2021_09_16_131012_create_reactions_table.php
+++ Board/database/migrations/2021_09_16_131012_create_reactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMessagesTable extends Migration
+class CreateReactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('reactions', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('board_id');
-            $table->string('user_name');
-            $table->string('message');
+            $table->integer('to_user_id');
+            $table->integer('from_user_id');
+            $table->integer('status');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('reactions');
     }
 }
--- Board/database/migrations/2021_09_16_131140_create_chat_room_users_table.php
+++ Board/database/migrations/2021_09_16_131140_create_chat_room_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBoardsTable extends Migration
+class CreateChatRoomUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateBoardsTable extends Migration
      */
     public function up()
     {
-        Schema::create('boards', function (Blueprint $table) {
+        Schema::create('chat_room_users', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('user_name');
-            $table->string('content');
+            $table->integer('chat_room_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateBoardsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('boards');
+        Schema::dropIfExists('chat_room_users');
     }
 }
